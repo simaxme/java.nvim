2141f30cc854ba91adb6f543f5bdabe6dbcd0c84
Agsayan <agsayan0@gmail.com>
2023-12-27 19:26:15 +0100

    introduce 'rename.root_markers' option to define a list of root markers

--- lua/java/rename/utils.lua
+++ lua/java/rename/utils.lua
@@ -20,71 +20,85 @@ function utils.split(search_string, pattern)
   return result
 end
 
+function utils.split_with_patterns(search_string, patterns)
+  local result = {}
+
+  for _, pattern in ipairs(patterns) do
+    result = utils.split(search_string, pattern)
+
+    if #result > 1 then
+      break
+    end
+    -- The pattern could not be found
+  end
+  return result
+end
+
 -- check if a path is a directory
 function utils.is_dir(path)
-    local f = io.open(path, "r")
-    local ok, err, code = f:read(1)
-    f:close()
-    return code == 21
+  local f = io.open(path, "r")
+  local ok, err, code = f:read(1)
+  f:close()
+  return code == 21
 end
 
 -- list all files in a folder using recursive
 function utils.list_folder_contents_recursive(folder)
-   local result = {}
+  local result = {}
 
-   local handle = io.popen("cd '" .. folder .. "' && find * -type f")
+  local handle = io.popen("cd '" .. folder .. "' && find * -type f")
 
-   while handle ~= nil do
-      local line = handle:read("l")
+  while handle ~= nil do
+    local line = handle:read("l")
 
-      if line == nil then
-         break
-      end
+    if line == nil then
+      break
+    end
 
-      table.insert(result, line)
-   end
+    table.insert(result, line)
+  end
 
-   return result
+  return result
 end
 
 -- list all files in a folder
 function utils.list_folder_contents(folder)
-   local result = {}
+  local result = {}
 
-   local command = "cd '" .. folder .. "' && find * -maxdepth 0 -type f"
-   local handle = io.popen(command)
+  local command = "cd '" .. folder .. "' && find * -maxdepth 0 -type f"
+  local handle = io.popen(command)
 
-   while handle ~= nil do
-      local line = handle:read("l")
+  while handle ~= nil do
+    local line = handle:read("l")
 
-      if line == nil then
-         break
-      end
+    if line == nil then
+      break
+    end
 
-      local l = string.find(line, "%.java$")
+    local l = string.find(line, "%.java$")
 
-      if l ~= nil then
-         table.insert(result, folder .. "/" .. line)
-      end
-   end
+    if l ~= nil then
+      table.insert(result, folder .. "/" .. line)
+    end
+  end
 
-   return result
+  return result
 end
 
 -- get the real/full path of the given path
 function utils.realpath(path)
-   local cmd = "realpath '" .. path .. "'"
-   local handle = io.popen(cmd)
+  local cmd = "realpath '" .. path .. "'"
+  local handle = io.popen(cmd)
 
-   if handle == nil then
-      return path
-   end
+  if handle == nil then
+    return path
+  end
 
-   local result = handle:read("*l")
-   
-   handle:close()
+  local result = handle:read("*l")
 
-   return result
+  handle:close()
+
+  return result
 end
 
 return utils
--- lua/java/snippets/init-file.lua
+++ lua/java/snippets/init-file.lua
@@ -5,6 +5,7 @@ local fmt = require("luasnip.extras.fmt").fmt
 local i = ls.insert_node
 local f = ls.function_node
 local utils = require("java.rename.utils")
+local options = require("java.rename.options")
 
 local function getShiftWidth()
     return vim.api.nvim_buf_get_option(0, "shiftwidth")
@@ -12,14 +13,11 @@ end
 
 local function getPackageName()
     local folder = vim.fn.expand("%:h")
+    local root_markers = options.get_rename_options().root_markers
 
-    local splitted = utils.split(folder, "main/java/")
+    local splitted = utils.split_with_patterns(folder, root_markers)
 
-    -- if main/java could not be found, try test/java
-    if #splitted <= 1 then
-        splitted = utils.split(folder, "test/java/")
-    end
-
+    -- if any of the root markers could not be found, cancel
     if #splitted <= 1 then
         return ""
     end
